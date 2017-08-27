46180a0a45ef3d884f8eb2726a0ad6cc199cc83b
Michael Spencer <sonrisesoftware@gmail.com>
2017-08-26 21:29:26 -0500

    Add disqus comments to the blog

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-  $(document).scroll(function() {
-    const bio = $('.bio')
-    const basePosition = bio.length > 0 ? bio.height() : 0
-    const isScrolled = $(document).scrollTop() > basePosition
-
-    $('.navbar').toggleClass('navbar--floating', isScrolled)
-  })
+  updateNavbar()
+  $(document).scroll(updateNavbar)
 })
+
+function updateNavbar() {
+  const bio = $('.bio')
+  const basePosition = bio.length > 0 ? bio.height() : 0
+  const isScrolled = $(document).scrollTop() > basePosition
+
+  $('.navbar').toggleClass('navbar--floating', isScrolled)
+}
